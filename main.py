f037fd5232658c9832cbffe0f93e0ff1a3d6d356
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2026-02-09 10:32:04 +0200

    not interesting
    (cherry picked from commit 58c18dd096c120c31440ec355317ac9a37069031)

--- main.py
+++ main.py
@@ -1,68 +1,69 @@
 import copy
-import gc
 import json
-import sys
+import os
 
 import orjson
 import math
-import os
 import pickle
 import time
 import tracemalloc
+import pinch
 
-from deserialize import load_bytes, decode_number
+import pinch_python
 
-from deserialize import Settings as LoadSettings
-
-os.environ["MSGPACK_PUREPYTHON"] = "True"
+# os.environ["MSGPACK_PUREPYTHON"] = "True"
 import msgpack
 
 from benchmark_displayer import display_benchmark
-from serialize import dump_bytes, Settings, encode_number
 
-
+# a = 1
+# print(load_bytes(pinch_python.dump_bytes(a)))
+# print(load_bytes(pinch_python.dump_bytes(a)) == a)
+# print(pinch_python.dump_bytes(a))
+# print(bytes(dump_bytes(a)))
+# exit()
 objects = [
-    # 1231,
-    # 1,
-    # 332634,
-    # 5437890568343289547384,
-    # 0,
-    # -1231,
-    # -1,
-    # -332634,
-    # -5437890568343289547384,
-    #
-    # 1.000000000001,
-    # 100000000000000000000000000000000000000000.1,
-    # 23423523.543262346234,
-    # 4.4,
-    # -1.000000000001,
-    # -100000000000000000000000000000000000000000.1,
-    # -23423523.543262346234,
-    # -4.4,
-    #
-    # math.nan,
-    # math.inf,
-    # -math.inf,
-    #
-    # "afsag",
-    # "092u384oiwjrklsgmfoisgjldkxfmoweij;lksgzwaoi;elgjskznwoi;jetlaksfdnv" * 1_000_000,
-    # "😎",
-    # "לא",
-    # "",
-    #
-    # b"1234",
-    # b"abcdefghijklmnopqrstuvwxyz",
-    # b"",
-    #
-    # None,
-    # True,
-    # False,
-    #
-    # [None] * 10,
+    1231,
+    1,
+    332634,
+    5437890568343289547384,
+    0,
+    -1231,
+    -1,
+    -332634,
+    -5437890568343289547384,
+
+    1.000000000001,
+    100000000000000000000000000000000000000000.1,
+    23423523.543262346234,
+    4.4,
+    -1.000000000001,
+    -100000000000000000000000000000000000000000.1,
+    -23423523.543262346234,
+    -4.4,
+
+    math.nan,
+    math.inf,
+    -math.inf,
+
+    "afsag",
+    "092u384oiwjrklsgmfoisgjldkxfmoweij;lksgzwaoi;elgjskznwoi;jetlaksfdnv" * 1_000_000,
+    "😎",
+    "לא",
+    "",
+
+    b"1234",
+    b"abcdefghijklmnopqrstuvwxyz",
+    b"",
+
+    None,
+    True,
+    False,
+
+    [None] * 10,
     [b"1234", b"asgsa", b"sgaeg4we"],
     [0.1, 0.2, 0.3, 0.4],
-    [-1, 0, 1, 2, 3],
+    [-91, 0, 1, 2, 3, 4, 5, 6, 7, 8],
     list(range(50, 1000)),
     ["aaaa", "aaaa", "aaaa"],
     [1, "asdg", b"234sa", 4.5, [1, 2, 3, 4, 5], False, [], None],
@@ -106,6 +107,7 @@ def profile(name: str, func: callable):
         c, peak = tracemalloc.get_traced_memory()
     except Exception as e:
         print(e)
+        # raise e
         return
     finally:
         tracemalloc.stop()
@@ -113,8 +115,14 @@ def profile(name: str, func: callable):
 
 
 for obj in objects:
-    serialized = dump_bytes(obj)
-    unserialized = load_bytes(serialized, settings=LoadSettings(modify_input=False))
+    print()
+    serialized = pinch_python.dump_bytes(obj, use_pointers=True)
+    # print(serialized)
+    # print(dump_bytes(obj))
+    unserialized = pinch_python.load_bytes(serialized, modify_input=False)
+    print()
+    print(str(obj)[:120])
+    print(str(unserialized)[:120])
     assert (isinstance(obj, float) and math.isnan(obj) and math.isnan(unserialized)) or unserialized == obj
 
     # serialized = dump_bytes(obj, settings=Settings(use_pointers=True))
@@ -122,43 +130,45 @@ for obj in objects:
     # assert (isinstance(obj, float) and math.isnan(obj) and math.isnan(unserialized)) or unserialized == obj
 # exit()
 
-# for obj in objects:
-#     print(str(obj)[:120])
-#     results = [
-#         profile("mine", lambda: dump_bytes(obj)),
-#         # profile("mine w pointer", lambda: dump_bytes(obj, settings=Settings(use_pointers=True))),
-#         profile("mine w pointer & str keys",
-#                lambda: dump_bytes(obj, settings=Settings(use_pointers=True, allow_non_string_keys=False))),
-#         profile("json", lambda: json.dumps(obj).encode()),
-#         profile("orjson", lambda: orjson.dumps(obj)),
-#         profile("pickle", lambda: pickle.dumps(obj)),
-#         profile("msgpack", lambda: msgpack.dumps(obj))
-#     ]
-#     # results.append(profile("str keys", lambda: dump_bytes(obj, settings=Settings(allow_non_string_keys=False))))
-#     results = [x for x in results if x]
-#
-#     display_benchmark(
-#         results,
-#         metrics={
-#             "elapsed": {
-#                 "label": "Elapsed (ms)",
-#                 "higher_is_better": False,
-#                 "format": "{:.2f}",
-#             },
-#             "end_len": {
-#                 "label": "End Len",
-#                 "higher_is_better": False,
-#                 "format": "{:.1f}",
-#             },
-#             "peak_memory": {
-#                 "label": "Peak Mem",
-#                 "higher_is_better": False,
-#                 "format": "{:.1f}",
-#             },
-#         },
-#     )
-#     # profile("msgpack", lambda: msgpack.dumps(obj))
+for obj in objects:
+    print(str(obj)[:120])
+    results = [
+        profile("pinch (python)", lambda: pinch_python.dump_bytes(obj)),
+        profile("pinch (rust)", lambda: pinch.dump_bytes(obj)),
+        profile("mine w pointer & str keys (python)",
+                lambda: pinch_python.dump_bytes(obj, use_pointers=True, allow_non_string_keys=False)),
+        profile("pinch w pointers (rust)", lambda: pinch.dump_bytes(obj, use_pointers=True)),
+        profile("json", lambda: json.dumps(obj).encode()),
+        profile("orjson", lambda: orjson.dumps(obj)),
+        profile("pickle", lambda: pickle.dumps(obj)),
+        profile("msgpack", lambda: msgpack.dumps(obj))
+    ]
+    # results.append(profile("str keys", lambda: dump_bytes(obj, settings=Settings(allow_non_string_keys=False))))
+    results = [x for x in results if x]
 
+    display_benchmark(
+        results,
+        metrics={
+            "elapsed": {
+                "label": "Elapsed (ms)",
+                "higher_is_better": False,
+                "format": "{:.2f}",
+            },
+            "end_len": {
+                "label": "End Len",
+                "higher_is_better": False,
+                "format": "{:.1f}",
+            },
+            "peak_memory": {
+                "label": "Peak Mem",
+                "higher_is_better": False,
+                "format": "{:.1f}",
+            },
+        },
+    )
+    # profile("msgpack", lambda: msgpack.dumps(obj))
+
+exit()
 for obj in objects:
     print(str(obj)[:120])
     mine = dump_bytes(obj)
