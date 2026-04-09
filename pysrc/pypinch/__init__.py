09783ebf0565ac55fbefe57a2330cceea74dc1c4
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2026-04-09 11:50:22 +0300

    add Idx to __init__

--- pysrc/pypinch/__init__.py
+++ pysrc/pypinch/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from .deserialize.lazy_load import lazy_load_bytes, bytes_check_if_contains
+from .deserialize.lazy_load import lazy_load_bytes, bytes_check_if_contains, Idx
 
 FORCE_PYTHON = os.environ.get("PYPINCH_FORCE_PYTHON")
 
